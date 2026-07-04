8b3b66cb552a033673e1a21ae769ba710d026cee
Evgenii Zorin <zorin.evgenii.m@gmail.com>
2026-07-04 10:05:53 -0600

    04.07.2026 update

--- query1.sql
+++
@@ -1,5 +0,0 @@
-SELECT column1, column2, keep, CASE WHEN surname IS NULL THEN 'Unknown' ELSE surname END AS surname
-FROM db1.table1
-WHERE column1 IN ('a', 'b', 'c')
-AND keep = 1
-;
--- query1_copy2.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    column1,
-    column2,
-    keep,
-    coalesce(surname, 'Unknown') AS surname
-FROM db1.table1
-WHERE
-    column1 IN ('a', 'b', 'c')
-    AND keep = 1;
